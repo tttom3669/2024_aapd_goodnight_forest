600055e9792bf668c71cdaeb6880749aa18402df
jhoujhou <a01450887@gmail.com>
2024-09-15 18:24:44 +0800

    更新睡眠待機頁面

--- main.js
+++ main.js
@@ -1,4 +1,17 @@
 import './assets/scss/all.scss';
 import 'bootstrap/dist/js/bootstrap.min.js';
-
 console.log("Hello world!");
+
+var swiper = new Swiper(".mySwiper", {
+  effect: 'coverflow',
+  slidesPerView: "auto",
+  spaceBetween: 24,
+  centeredSlides: true,
+  loop: "infinite",
+  coverflowEffect: {
+    depth: 200,
+    rotate: 30,
+    stretch: 0,
+    slideShadows: false,
+},
+});
