25de428c0a2d13157e021aaab1eac28a897f1f30
tttom3669 <tttom3669@gmail.com>
2024-10-03 11:42:28 +0800

    Updates

---
+++ assets/edit-reservation-info-18531260.js
@@ -0,0 +1,1 @@
+import"./main-75159efb.js";const e=document.querySelector('input[name="datepicker"]');new Datepicker(e,{autohide:!0,language:"zh-CN"});
--- assets/edit-reservation-info-3a182e55.js
+++
@@ -1,1 +0,0 @@
-import"./main-6b83d44d.js";const e=document.querySelector('input[name="datepicker"]');new Datepicker(e,{autohide:!0,language:"zh-CN"});
